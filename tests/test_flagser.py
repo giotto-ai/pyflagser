41a926760c31bafad282d019acce8bdb2a1a0d0f
Guillaume Tauzin <g.tauzin@l2f.ch>
2020-01-07 17:42:41 +0100

    [WIP] Updated tests

--- tests/test_flagser.py
+++ tests/test_flagser.py
@@ -2,42 +2,44 @@
 # License : Apache 2.0
 
 import numpy as np
+import scipy.sparse as sp
 import os
 import pytest
-from flagser_binding import flagser, flagser_file
+from numpy.testing import assert_almost_equal
 
+from pyflagser import loadflag, saveflag, flagser
 
-def test_flagser_on_file_works():
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, 'large-test-data.flag')
+flag_files = []
 
-    ret = flagser_file(filename)
-    assert len(ret) == 1
-    assert ret[0]['betti'] == [0, 90999, 378, 0]
+dirname = os.path.join(os.path.dirname(__file__), "../flagser/test")
+for file in os.listdir(dirname):
+    if file.endswith(".flag"):
+        flag_files.append(os.path.join(dirname, file))
 
+print(flag_files)
 
-def test_flagser_works():
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, 'large-test-data.flag')
+def test_flagio():
+    for flag_file in flag_files:
+        flag_matrix = loadflag(flag_file)
+        _, fname_temp = os.path.split(flag_file)
+        saveflag(fname_temp, flag_matrix)
+        flag_matrix_temp = loadflag(fname_temp)
+        print(flag_matrix)
+        print('-----------')
+        print(flag_matrix_temp)
 
-    is_dim0 = True
-    edges = []
-    # Convert flagser format file to numpy arrays
-    with open(filename, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line == 'dim 1':
-                is_dim0 = False
-                continue
-            if line == 'dim 0':
-                continue
+        assert_almost_equal(flag_matrix.diagonal(), flag_matrix_temp.diagonal())
+        assert_almost_equal(np.sort(np.hstack([sp.find(flag_matrix)])),
+                            np.sort(np.hstack([sp.find(flag_matrix_temp)])))
+#        break
 
-            if is_dim0:
-                vertices = np.asarray(list(map(float, line.split(' '))))
-            else:
-                edges.append(list(map(float, line.split(' '))))
+# def test_flagser():
+#     dirname = os.path.dirname(__file__)
+#     filename = os.path.join(dirname, 'large-test-data.flag')
 
-    edges = np.asarray(edges)
-    ret = flagser(vertices, edges)
-    assert len(ret) == 1
-    assert ret[0]['betti'] == [0, 90999, 378, 0]
+#     flag_matrix = loadflag(filename)
+
+#     ret = flagser(flag_matrix)
+#     print('The H0 value is different from what is shown in stdout: ', ret['betti'])
+#     assert ret['betti'] == [0, 90999, 378, 0]
+#     # assert False
