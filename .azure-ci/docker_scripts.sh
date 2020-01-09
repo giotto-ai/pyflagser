5e971c63619eb8b20c7653f14a78741e68f02a78
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-01-09 10:47:32 +0100

    Update docker script for pyflagser

--- .azure-ci/docker_scripts.sh
+++ .azure-ci/docker_scripts.sh
@@ -10,24 +10,10 @@ pip install --upgrade pip setuptools
 # installing cmake
 pip install cmake
 
-# installing boost
-yum install -y wget tar
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
-tar -zxvf /boost_1_69_0.tar.gz
-mkdir boost
-cd /boost_1_69_0
-./bootstrap.sh --prefix=/boost
-./b2 install
-cd ..
-
-# helping cmake find boost
-export BOOST_ROOT=/boost
-export Boost_INCLUDE_DIR=/boost/include
-
-# installing and uninstalling giotto-learn
+# installing and uninstalling pyflagser
 cd /io
 pip install -e ".[doc, tests]"
-pip uninstall -y giotto-learn
+pip uninstall -y pyflagser
 
 # testing, linting
 pytest --cov . --cov-report xml
