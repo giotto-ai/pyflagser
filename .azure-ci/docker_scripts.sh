160ec2415a148a5a416ba14ed0d38618fa4068c6
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2020-02-07 19:50:29 +0100

    Downgrade pip version

--- .azure-ci/docker_scripts.sh
+++ .azure-ci/docker_scripts.sh
@@ -5,7 +5,7 @@ set -x
 # upgrading pip and setuptools
 PYTHON_PATH=$(eval find "/opt/python/*${python_ver}*" -print)
 export PATH=${PYTHON_PATH}/bin:${PATH}
-pip install --upgrade pip setuptools
+pip install --upgrade pip==19.3.1 setuptools
 
 # installing cmake
 pip install cmake
