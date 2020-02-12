0bc2daf7d02853d0f0ab09bdbfd71a409d4e3ab0
Umberto <u.lupo@l2f.ch>
2020-02-12 14:29:50 +0100

    Try new approach to avoid issues with --pyargs and command line arguments

--- pyflagser/tests/test_flagio.py
+++ pyflagser/tests/test_flagio.py
@@ -1,42 +1,20 @@
 """Testing for the python bindings of the C++ flagser library."""
 
 import os
-from tempfile import mkdtemp
-from urllib.request import urlopen, urlretrieve
 
 import numpy as np
 import pytest
 import scipy.sparse as sp
+from fetch_flag_files import fetch_flag_files
 from numpy.testing import assert_almost_equal
 
 from pyflagser import loadflag, saveflag
 
 
-def list_flag_files(webdl):
-    if not webdl:
-        dirname = os.path.join(os.path.dirname(__file__), "../../flagser/test")
-        flag_files = [os.path.join(dirname, fname)
-                      for fname in os.listdir(dirname)
-                      if fname.endswith(".flag")]
-    else:
-        # Download from remote bucket
-        temp_dir = mkdtemp()
-        bucket_url = 'https://storage.googleapis.com/l2f-open-models/' \
-                     'giotto-tda/flagser/test/'
-        flag_files_list = bucket_url + 'flag_files_list.txt'
-        with urlopen(flag_files_list) as f:
-            flag_file_names = f.read().decode('utf8').splitlines()
-            flag_files = []
-            for fname in flag_file_names:
-                url = bucket_url + fname
-                fpath = os.path.join(temp_dir, fname)
-                urlretrieve(url, fpath)
-                flag_files.append(fpath)
-    return flag_files
-
-
 def test_super(webdl):
-    @pytest.mark.parametrize("flag_file", list_flag_files(webdl))
+    flag_files = fetch_flag_files(webdl)
+
+    @pytest.mark.parametrize('flag_file', flag_files)
     def test_flagio(flag_file):
         flag_matrix = loadflag(flag_file)
         _, fname_temp = os.path.split(flag_file)
--- pyflagser/tests/test_flagser.py
+++ pyflagser/tests/test_flagser.py
@@ -1,15 +1,14 @@
 """Testing for the python bindings of the C++ flagser library."""
 
 import os
-from tempfile import mkdtemp
-from urllib.request import urlretrieve
 
 import pytest
+from fetch_flag_files import fetch_flag_files
 from numpy.testing import assert_almost_equal
 
 from pyflagser import loadflag, flagser
 
-betti = {
+betti_exp = {
     'a.flag': [1, 2, 0],
     'b.flag': [1, 0, 0],
     'c.flag': [1, 5],
@@ -29,34 +28,16 @@ betti = {
     'd10.flag': [1, 0, 0, 0, 0, 0, 0, 0, 0, 1334961],
 }
 
-# try:
-#     dirname = os.path.join(os.path.dirname(__file__), "../../flagser/test")
-#     list_dir = os.listdir(dirname)
-#     flag_files = [os.path.join(dirname, fname)
-#                   for fname in os.listdir(dirname)
-#                   if fname.endswith(".flag")]
-#     download_files = False
-# except FileNotFoundError:
-#     # Download from remote bucket
-#     temp_dir = mkdtemp()
-#     bucket_url = 'https://storage.googleapis.com/l2f-open-models/giotto-tda' \
-#                  '/flagser/test/'
-#     flag_files = []
-#     for fname in betti.keys():
-#         url = bucket_url + fname
-#         fpath = os.path.join(temp_dir, fname)
-#         urlretrieve(url, fpath)
-#         flag_files.append(fpath)
-#     download_files = True
-#
-#
-# @pytest.mark.parametrize("flag_file, betti",
-#                          [(flag_file, betti[os.path.split(flag_file)[1]])
-#                           for flag_file in flag_files
-#                           if os.path.split(flag_file)[1] in betti.keys()])
-# def test_flagser(flag_file, betti):
-#     flag_matrix = loadflag(flag_file)
-#
-#     ret = flagser(flag_matrix)
-#     assert_almost_equal(ret['betti'], betti)
 
+def test_super(webdl):
+    flag_files = fetch_flag_files(webdl)
+
+    @pytest.mark.parametrize(
+        'flag_file, betti',
+        [(flag_file, betti_exp[os.path.split(flag_file)[1]])
+         for flag_file in flag_files
+         if os.path.split(flag_file)[1] in betti_exp.keys()])
+    def test_flagser(flag_file, betti):
+        flag_matrix = loadflag(flag_file)
+        ret = flagser(flag_matrix)
+        assert_almost_equal(ret['betti'], betti_exp)
