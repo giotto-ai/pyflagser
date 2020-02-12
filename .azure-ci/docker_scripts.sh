76a658c2f76c321e47e65ebf366351bb597f370e
Umberto <u.lupo@l2f.ch>
2020-02-12 17:08:41 +0100

    Remove contradictory passed options

--- .azure-ci/docker_scripts.sh
+++ .azure-ci/docker_scripts.sh
@@ -15,7 +15,7 @@ cd /io
 pip install -e ".[doc, tests]"
 
 # Test dev install with pytest and flake8
-pytest --cov pyflagser --no-cov --no-coverage-upload
+pytest pyflagser --no-cov --no-coverage-upload
 flake8 --exit-zero /io/
 
 # Uninstal pyflagser dev
