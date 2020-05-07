3449bf3b93e619f579369798d8d5dec580615672
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2020-05-07 12:56:00 +0200

    Add flagser_static and flagser_persistence
    Signed-off-by: Guillaume Tauzin <guillaumetauzin.ut@gmail.com>

--- pyflagser/__init__.py
+++ pyflagser/__init__.py
@@ -1,6 +1,7 @@
 from ._version import __version__
 
 from .flagio import loadflag, saveflag
-from .flagser import flagser
+from .flagser import flagser_static, flagser_persistence
 
-__all__ = ['loadflag', 'saveflag', 'flagser', '__version__']
+__all__ = ['loadflag', 'saveflag', 'flagser_static', 'flagser_persistence',
+           '__version__']
