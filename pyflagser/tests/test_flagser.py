323d2fe87627b5798d702a1cfb1f20873e6a2cc9
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2020-05-11 17:18:31 +0200

    Fix wrong cell_count and dgms list length (#35)
    * Fetch flagser fix
    
    * Extend tests for cell_count
    
    Signed-off-by: Guillaume Tauzin <guillaumetauzin.ut@gmail.com>

--- pyflagser/tests/test_flagser.py
+++ pyflagser/tests/test_flagser.py
@@ -27,6 +27,29 @@ betti = {
     'd10.flag': [1, 0, 0, 0, 0, 0, 0, 0, 0, 1334961],
 }
 
+
+cell_count = {
+    'a.flag': [5, 7, 1],
+    'b.flag': [4, 6, 3],
+    'c.flag': [8, 12],
+    'd.flag': [5, 9, 6],
+    'e.flag': [5, 9, 7, 2],
+    'f.flag': [3, 5, 3],
+    'd2.flag': [2, 2],
+    'd3.flag': [3, 6, 6],
+    'd3-allzero.flag': [3, 6, 6],
+    'double-d3.flag': [4, 10, 12],
+    'double-d3-allzero.flag': [4, 10, 12],
+    'd4.flag': [4, 12, 24, 24],
+    'd4-allzero.flag': [4, 12, 24, 24],
+    'd5.flag': [5, 20, 60, 120, 120],
+    'd7.flag': [7, 42, 210, 840, 2520, 5040, 5040],
+    'medium-test-data.flag': [31346, 68652, 12694, 250],
+    'd10.flag': [10, 90, 720, 5040, 30240, 151200, 604800,
+                 1814400, 3628800, 3628800],
+}
+
+
 # Filtrations are only tested for d5.flag, see conftest.py
 filtrations_results = {
     'dimension':
@@ -175,11 +198,17 @@ def are_matrices_equal(m1, m2):
     return True
 
 
-def test_betti(flag_file):
-    betti_exp = betti[os.path.split(flag_file)[1]]
+def test_output(flag_file):
     flag_matrix = loadflag(flag_file, fmt='coo')
-    betti_res = flagser(flag_matrix)["betti"]
+    res = flagser(flag_matrix)
+
+    betti_exp = betti[os.path.split(flag_file)[1]]
+    betti_res = res["betti"]
     assert_almost_equal(betti_res, betti_exp)
+
+    cell_count_exp = cell_count[os.path.split(flag_file)[1]]
+    cell_count_res = res["cell_count"]
+    assert_almost_equal(cell_count_res, cell_count_exp)
 
 
 def test_filtrations_d5(flag_file, filtration):
