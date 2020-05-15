f92fd1536cde344445de106be8f897f936ea0e6e
Guillaume Tauzin <guillaumetauzin.ut@gmail.com>
2020-05-16 01:03:19 +0200

    Add flagser_count bindings
    Signed-off-by: Guillaume Tauzin <guillaumetauzin.ut@gmail.com>

---
+++ src/flagser_count_bindings.cpp
@@ -0,0 +1,107 @@
+// #define WITH_HDF5
+// #define KEEP_FLAG_COMPLEX_IN_MEMORY
+// #define USE_COEFFICIENTS
+// #define MANY_VERTICES
+#include <stdio.h>
+#include <iostream>
+
+#include <flagser/include/argparser.h>
+#include <flagser/src/flagser-count.cpp>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(flagser_count_pybind, m) {
+  m.def("compute_cell_count", [](std::vector<value_t>& vertices,
+                                 std::vector<std::vector<value_t>>& edges,
+                                 unsigned short min_dim, short max_dim,
+                                 bool directed, std::string filtration) {
+    // Save std::cout status
+    auto cout_buff = std::cout.rdbuf();
+    named_arguments_t named_arguments;
+    std::string str_max;
+    std::string str_min;
+
+    HAS_EDGE_FILTRATION has_edge_filtration =
+        HAS_EDGE_FILTRATION::TOO_EARLY_TO_DECIDE;
+
+    unsigned short effective_max_dim = max_dim;
+    std::string default_filtration = "max";
+
+    if (max_dim < 0)
+      effective_max_dim = std::numeric_limits<unsigned short>::max();
+
+    str_max = std::to_string(effective_max_dim);
+    str_min = std::to_string(min_dim);
+
+    named_arguments["out"] = "output_flagser_file";
+    named_arguments["max-dim"] = str_max.c_str();
+    named_arguments["min-dim"] = str_min.c_str();
+
+    // Is filtration supported ?
+    if (std::find(custom_filtration_computer.begin(),
+                  custom_filtration_computer.end(),
+                  filtration) == custom_filtration_computer.end()) {
+      std::cout << filtration << " not found, fallback to "
+                << default_filtration << "\n";
+      filtration = default_filtration;
+
+      std::cout << "Implemented filtrations:\n";
+      for (auto& elem : custom_filtration_computer) {
+        if (elem != custom_filtration_computer.back())
+          std::cout << elem << ", ";
+        else
+          std::cout << elem << "\n";
+      }
+    }
+
+    named_arguments["filtration"] = filtration.c_str();
+
+    remove(named_arguments["out"]);
+
+    auto graph = filtered_directed_graph_t(vertices, directed);
+
+    // If we have at least one vertice
+    if (edges.size() && has_edge_filtration == HAS_EDGE_FILTRATION::MAYBE) {
+      // If the edge has three components, then there are also
+      // filtration values, which we assume to come last
+      has_edge_filtration = edges[0].size() == 2 ? HAS_EDGE_FILTRATION::NO
+                                                 : HAS_EDGE_FILTRATION::YES;
+    }
+
+    for (auto& edge : edges) {
+      if (has_edge_filtration == NO) {
+        graph.add_edge(edge[0], edge[1]);
+      } else {
+        if (edge[2] < std::max(vertices[edge[0]], vertices[edge[1]])) {
+          std::cerr << "The data contains an edge "
+                       "filtration that contradicts the vertex "
+                       "filtration, the edge ("
+                    << edge[0] << ", " << edge[1] << ") has filtration value "
+                    << edge[2] << ", which is lower than min("
+                    << vertices[edge[0]] << ", " << vertices[edge[1]]
+                    << "), the filtrations of its edges.";
+          exit(-1);
+        }
+        graph.add_filtered_edge((vertex_index_t)edge[0],
+                                (vertex_index_t)edge[1], edge[2]);
+      }
+    }
+
+    std::cout.rdbuf(nullptr);
+
+    auto ret = count_cells(graph, named_arguments);
+
+    if (remove(named_arguments["out"]) != 0)
+      perror("Error deleting flagser output file");
+
+    // re enable again cout
+    std::cout.rdbuf(cout_buff);
+
+    return ret;
+  });
+
+  m.doc() = "Python bindings for flagser_count";
+}
