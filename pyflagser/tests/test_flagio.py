aaed4dd5d26771d3fca7195538c1dd3b27921710
Umberto Lupo <umberto.lupo@gmail.com>
2020-11-27 14:10:11 +0100

    Restrict some tests to small files only

--- pyflagser/tests/test_flagio.py
+++ pyflagser/tests/test_flagio.py
@@ -12,21 +12,21 @@ from pyflagser._utils import _extract_unweighted_graph, \
     _extract_weighted_graph
 
 
-def test_unweighted_fmt(flag_file):
-    adjacency_matrix = load_unweighted_flag(flag_file, fmt='coo')
+def test_unweighted_fmt(flag_file_small):
+    adjacency_matrix = load_unweighted_flag(flag_file_small, fmt='coo')
     vertices_coo, edges_coo = _extract_unweighted_graph(adjacency_matrix)
-    adjacency_matrix = load_unweighted_flag(flag_file, fmt='dense')
+    adjacency_matrix = load_unweighted_flag(flag_file_small, fmt='dense')
     vertices_dense, edges_dense = _extract_unweighted_graph(adjacency_matrix)
     assert_almost_equal(vertices_coo, vertices_dense)
     assert_almost_equal(edges_coo, edges_dense)
 
 
 @pytest.mark.parametrize('max_edge_length', [0.1, 0.5, np.inf])
-def test_weighted(flag_file, max_edge_length):
-    adjacency_matrix = load_weighted_flag(flag_file)
+def test_weighted(flag_file_small, max_edge_length):
+    adjacency_matrix = load_weighted_flag(flag_file_small)
     vertices_a, edges_a = _extract_weighted_graph(adjacency_matrix,
                                                   max_edge_length)
-    fname_temp = os.path.split(flag_file)[1]
+    fname_temp = os.path.split(flag_file_small)[1]
     save_weighted_flag(fname_temp, adjacency_matrix)
     adjacency_matrix = load_weighted_flag(fname_temp)
     vertices_b, edges_b = _extract_weighted_graph(adjacency_matrix,
