226dfa70370d966e034c9dcd5ea1a700b370826a
julian <julian.burellaperez@heig-vd.ch>
2021-04-22 12:07:31 +0200

    [PYTHON] Add Try/catch block inside of __init__.py
    Allows to import pyflagser package without installing the package + dependencies
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- pyflagser/__init__.py
+++ pyflagser/__init__.py
@@ -1,17 +1,24 @@
 from ._version import __version__
 
-from .flagio import load_unweighted_flag, load_weighted_flag, \
-    save_unweighted_flag, save_weighted_flag
-from .flagser import flagser_unweighted, flagser_weighted
-from .flagser_count import flagser_count_unweighted, \
-    flagser_count_weighted
+__all__ = ['__version__']
 
-__all__ = ['load_unweighted_flag',
-           'load_weighted_flag',
-           'save_unweighted_flag',
-           'save_weighted_flag',
-           'flagser_unweighted',
-           'flagser_weighted',
-           'flagser_count_unweighted',
-           'flagser_count_weighted',
-           '__version__']
+# Allows to have access to the version information without
+# installating package + dependencies
+try:
+    from .flagio import load_unweighted_flag, load_weighted_flag, \
+        save_unweighted_flag, save_weighted_flag
+    from .flagser import flagser_unweighted, flagser_weighted
+    from .flagser_count import flagser_count_unweighted, \
+        flagser_count_weighted
+
+    __all__ += ['load_unweighted_flag',
+                'load_weighted_flag',
+                'save_unweighted_flag',
+                'save_weighted_flag',
+                'flagser_unweighted',
+                'flagser_weighted',
+                'flagser_count_unweighted',
+                'flagser_count_weighted'
+                ]
+except Exception:
+    pass
