b8f6f735d793977b7a256d9e79847194adc85a99
julian <julian.burellaperez@heig-vd.ch>
2021-04-22 12:00:29 +0200

    [DOC] Fix warning issued by error E402 for python
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import sys
 # the documentation
 sys.path.insert(0, os.path.abspath('../'))
 
-from pyflagser import __version__
+from pyflagser import __version__ # noqa
 
 
 # -- Project information -----------------------------------------------------
