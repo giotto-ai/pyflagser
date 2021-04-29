9639ffe261e051c5ee337017287de549306a6235
flomlo <florian.unger@fau.de>
2021-04-29 10:18:53 +0200

    Update pyflagser/flagio.py
    Co-authored-by: Umberto Lupo <46537483+ulupo@users.noreply.github.com>

--- pyflagser/flagio.py
+++ pyflagser/flagio.py
@@ -192,7 +192,7 @@ def save_unweighted_flag(fname, adjacency_matrix):
 
     with open(fname, 'w') as f:
         np.savetxt(f, vertices.reshape((1, -1)), delimiter=' ', comments='', 
-                header='dim 0', fmt='%i')
+                   header='dim 0', fmt='%i')
         np.savetxt(f, edges, comments='', header='dim 1', fmt='%i %i %i')
 
 
