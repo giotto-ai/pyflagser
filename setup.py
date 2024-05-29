bf2d9acee5db2dde1832e776e7dadd90398d847a
matteocao <38946719+matteocao@users.noreply.github.com>
2024-05-29 23:57:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -48,7 +48,8 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.7',
                'Programming Language :: Python :: 3.8',
                'Programming Language :: Python :: 3.9',
-               'Programming Language :: Python :: 3.10']
+               'Programming Language :: Python :: 3.10',
+               'Programming Language :: Python :: 3.11']
 KEYWORDS = 'topological data analysis, persistent ' + \
     'homology, directed flags complex, persistence diagrams'
 INSTALL_REQUIRES = requirements
